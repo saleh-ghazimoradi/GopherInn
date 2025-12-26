fe0fbfd1bf32c4cab3b9f5916a749d12851b34f9
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-12-26 14:23:35 +0330

    establish http server

---
+++ internal/server/server.go
@@ -0,0 +1,127 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/rs/zerolog"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type Server struct {
+	Host         string
+	Port         string
+	Handler      http.Handler
+	IdleTimeout  time.Duration
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	ErrorLog     *log.Logger
+	logger       *zerolog.Logger
+}
+
+type Options func(*Server)
+
+func WithHost(host string) Options {
+	return func(s *Server) {
+		s.Host = host
+	}
+}
+
+func WithPort(port string) Options {
+	return func(s *Server) {
+		s.Port = port
+	}
+}
+
+func WithHandler(h http.Handler) Options {
+	return func(s *Server) {
+		s.Handler = h
+	}
+}
+
+func WithIdleTimeout(idleTimeout time.Duration) Options {
+	return func(s *Server) {
+		s.IdleTimeout = idleTimeout
+	}
+}
+
+func WithReadTimeout(readTimeout time.Duration) Options {
+	return func(s *Server) {
+		s.ReadTimeout = readTimeout
+	}
+}
+
+func WithWriteTimeout(writeTimeout time.Duration) Options {
+	return func(s *Server) {
+		s.WriteTimeout = writeTimeout
+	}
+}
+
+func WithLogger(logger *zerolog.Logger) Options {
+	return func(s *Server) {
+		s.logger = logger
+	}
+}
+
+func WithErrorLog(logger *log.Logger) Options {
+	return func(s *Server) {
+		s.ErrorLog = logger
+	}
+}
+
+func (s *Server) Connect() error {
+	addr := fmt.Sprintf("%s:%s", s.Host, s.Port)
+	server := &http.Server{
+		Addr:         addr,
+		Handler:      s.Handler,
+		IdleTimeout:  s.IdleTimeout,
+		ReadTimeout:  s.ReadTimeout,
+		WriteTimeout: s.WriteTimeout,
+		ErrorLog:     s.ErrorLog,
+	}
+
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		se := <-quit
+
+		s.logger.Info().Str("signal", se.String()).Msg("caught shutdown signal")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		err := server.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+		s.logger.Info().Str("addr", server.Addr).Msg("completing background task")
+		shutdownError <- nil
+	}()
+
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	if err := <-shutdownError; err != nil {
+		return err
+	}
+
+	s.logger.Info().Str("addr", addr).Msg("server stopped")
+
+	return nil
+}
+
+func NewServer(opts ...Options) *Server {
+	s := &Server{}
+	for _, opt := range opts {
+		opt(s)
+	}
+	return s
+}
